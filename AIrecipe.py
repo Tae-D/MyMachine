14efb2271730a651dc7e1805309c8254bf65ed0c
SummerShelf <timo.shuvalov@gmail.com>
2025-12-22 21:51:38 +0100

    změnil jsem prompt a lepší input

--- AIrecipe.py
+++ AIrecipe.py
@@ -5,18 +5,16 @@ import json
 BASEURL= "https://aihorde.net/api/v2"
 API_KEY = "WqioktBxymwd88nxXpuvAQ"
 
-headersrecipe = {
-    "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNzY2NDA3Mjc2LCJleHAiOjE3NjY0OTM2NzYsInJlbWVtYmVyIjpmYWxzZX0.ltFexg4xpnWV1nuik7ZKw9-oYAozzzf0YJlXxDgzooCnv4bvu_U6unKEFDf8Txz9yOcrMAqfRM6cdzTN8mBnbA"
-}
+
 
 
 def get_job_id(prompt:str):
-    url=f"{BASEURL}/generate/text/async"
+    urlai=f"{BASEURL}/generate/text/async"
     payload = {"n":1,"max_length": 10000,"prompt":prompt}
     headers ={}
     if API_KEY:
         headers["apikey"] = API_KEY
-    response= requests.post(url, json=payload, headers=headers)
+    response= requests.post(urlai, json=payload, headers=headers)
 
     if response.status_code!=200 and response.status_code!=202:
         print(f"error code: {response.status_code}, error message: {response.text}")
@@ -30,35 +28,39 @@ def get_answer(job):
     attempt=0
     while True:
         r = requests.get(status_url)
-        print(r.json().get("wait_time"))
         if r.status_code!=200 and r.status_code!=202:
             print(f"error code: {r.status_code}, error message: {r.text}")
+
         if r.json().get("finished"):
             absoluteresponse=r.json()["generations"][0]["text"]
             return absoluteresponse
-        time.sleep(1)
+        time.sleep(2)
         attempt+=1
+        print(r.json().get("wait_time"),r.json().get("is_possible"))
+
         if attempt>30:
             print("too many attempts")
             return False
 
-def main(prompt:str):
+def main(prompt:str, urlrecipe:str, headersrecipefunc):
     attempt_generate=0
-    while True:
-        recipeconnectioncode=requests.get("https://demo.cocktailpi.org/api/recipe/?page=1&inCategory=1",headers=headersrecipe)
-        if recipeconnectioncode.status_code ==200:
-            print("connected to recipe database")
-            recipes=recipeconnectioncode.json()
-        else:
-            print(f"Couldn't connect to recipe database, try again later. Error code: {recipeconnectioncode.status_code}")
+    recipeconnectioncode=requests.get(urlrecipe, headers=headersrecipefunc)
+    if recipeconnectioncode.status_code ==200:
+        recipes=recipeconnectioncode.json()
+    else:
+        print(f"Couldn't connect to recipe database, try again later. Error code: {recipeconnectioncode.status_code}")
+        exit(2)
 
+    while True:
         system_instruction = (
             f"You are a drink machine API. Respond ONLY in valid dictionary. Take one drink from {recipes}. "
-            "The JSON must contain three keys: 'reply', 'suggestedrecipeId',  'reasoningSummary', 'replytouser'."
+            "The dictionary must contain four keys: 'reply', 'suggestedRecipeId',  'reasoningSummary', 'humanResponse'."
+            "humanResponse should be a comment in Czech for drink, that is chosen"
+
         )
         example = (
             "Example Input: 'I am thirsty'\n"
-            "Example Output: {\"reply\": \"Rockshandy\", \"suggestedrecipeId\": 251, \"reasoningSummary\": \"User needs something against thirst.\", \"replytouser\": Write a text in czech to user about their drink}"
+            "Example Output: {\"reply\": \"Rockshandy\", \"suggestedRecipeId\": 251, \"reasoningSummary\": \"User needs something against thirst.\", \"humanResponse\": \"Tady je tvoje Rockshandy, obsahuje citrónovou sodu, která je výborná proti žízni\"}"
         )
 
         finalprompt = f"{system_instruction}\n\n{example}\n\nInput: {prompt}\nOutput:"
@@ -69,25 +71,33 @@ def main(prompt:str):
             continue
         start=absoluteresponse.find("{")
         end=absoluteresponse.find("}")
-        print(absoluteresponse)
+
         if start==-1 or end==-1:
-            print("bad json response")
+            print(f"bad json response TEXT: {absoluteresponse}")
             continue
-        else:
+        try:
             res = json.loads(absoluteresponse[start:end+1])
+        except json.JSONDecodeError:
+            print(f"AI sent broken JSON TEXT: {absoluteresponse}")
+            continue
+        recipes_list=[recipes["content"][i]["name"] for i in range(len(recipes["content"]))]
+        if res.get("reply") in recipes_list:
             print(res)
-            recipes_list=[recipes["content"][i]["name"] for i in range(len(recipes["content"]))]
-            if res.get("reply") in recipes_list:
-                print(res.get("reply"))
-                return(res)
+            print(res.get("reply"))
+            return res
+        else:
+            attempt_generate+=1
+            if attempt_generate>3:
+                print("too many attempts, try diferent prompt")
+                exit()
             else:
-                attempt_generate+=1
-                if attempt_generate>3:
-                    print("too many attempts, try diferent prompt")
-                    exit()
-                else:
-                    print("drink is not detected")
-                    continue
+                print(f"drink is not detected TEXT: {absoluteresponse}")
+                continue
 
 if __name__ == '__main__':
-    main("chci něco tropického")
+    url="https://demo.cocktailpi.org/api/recipe/?page=1&inCategory=1"
+    headersrecipe={"Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNzY2NDA3Mjc2LCJleHAiOjE3NjY0OTM2NzYsInJlbWVtYmVyIjpmYWxzZX0.ltFexg4xpnWV1nuik7ZKw9-oYAozzzf0YJlXxDgzooCnv4bvu_U6unKEFDf8Txz9yOcrMAqfRM6cdzTN8mBnbA"}
+
+    promptmain="I am sad"
+
+    main(promptmain,url,headersrecipe)
