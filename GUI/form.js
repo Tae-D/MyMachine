06ee9b635636f609e26ae29ea7b927e6d51bf248
Tae-D <raspberry.tadeas@gmail.com>
2026-01-14 19:40:41 +0100

    přídáno css - nedokončeno

---
+++ GUI/form.js
@@ -0,0 +1,22 @@
+const form = document.getElementById("promptform");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const promptValue = document.getElementById("prompt").value;
+  form.innerHTML = "<fieldset disabled>" + form.innerHTML;
+  try {
+    // "http://localhost:5000/process" je adresa vašeho Python serveru
+    const response = await fetch("http://localhost:5000/process", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text: promptValue }),
+    });
+    const data = await response.json();
+    document.getElementById("response").innerHTML =
+      "Odpověď: " + data.humanResponse + "<br>" + "Nápoj : " + data.reply;
+  } catch (error) {
+    console.error("Chyba:", error);
+  }
+});
